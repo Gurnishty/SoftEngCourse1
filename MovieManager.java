9300dae08172478a534ca5d8e56e402465b71dd9
Ethantication <ethanorjoseph@gmail.com>
2025-04-29 16:25:50 +0300

    Update MovieManager.java

--- MovieManager.java
+++ MovieManager.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class MovieManager {
     ArrayList<Movie> movies = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
+
     public void addMovie() {
         System.out.println("Enter movie name:");
         String title = scanner.nextLine();
@@ -28,85 +29,74 @@ public class MovieManager {
         movies.add(movie);
 
         System.out.println("Movie added successfully!");
-
     }
-    public void displayAllMovies(){
-        if(movies.isEmpty()){
+
+    public void displayAllMovies() {
+        if (movies.isEmpty()) {
             System.out.println("No movies are available");
             return;
-        } else{
-            for(Movie movie : movies){
-                System.out.println("Name: " + movie.title + ",rating: " + movie.rating + ",director: " + movie.director);
+        } else {
+            for (Movie movie : movies) {
+                System.out.println("Name: " + movie.title + ", rating: " + movie.rating + ", director: " + movie.director);
             }
         }
-
     }
 
-    public void displayMovieRating(String title) {
+    public void displayMovieRating() {
         System.out.println("Enter movie name:");
+        scanner.nextLine();
         String movie_title = scanner.nextLine();
         for (Movie movie : movies) {
             if (movie.title.equalsIgnoreCase(movie_title)) {
-                System.out.println("Rating for" + movie.title + " : " + movie.rating);
+                System.out.println("Rating for " + movie.title + " : " + movie.rating);
                 return;
-
             }
         }
         System.out.println("Movie not found with name " + movie_title);
     }
 
-
     public void findBestDirector() {
-        if(movies.isEmpty()){
+        if (movies.isEmpty()) {
             System.out.println("No movies are available.");
-        } else {String[] directors_unique = new String[movies.size()];
-            int[] count_each_director = new int[movies.size()];
-            int size = 0;
+        } else {
+            String[] directors = new String[movies.size()];
+            double[] ratingSums = new double[movies.size()];
+            int[] movieCounts = new int[movies.size()];
+            int uniqueDirectorCount = 0;
 
+            for (Movie movie : movies) {
+                String director = movie.director;
+                boolean found = false;
 
-            String director = "some director name";
+                for (int i = 0; i < uniqueDirectorCount; i++) {
+                    if (directors[i].equals(director)) {
+                        ratingSums[i] += movie.rating;
+                        movieCounts[i]++;
+                        found = true;
+                        break;
+                    }
+                }
 
-            boolean found = false;
-            for (int i = 0; i < size; i++) {
-                if (directors_unique[i].equals(director)) {
-                    count_each_director[i]++;
-                    found = true;
-                    break;
+                if (!found) {
+                    directors[uniqueDirectorCount] = director;
+                    ratingSums[uniqueDirectorCount] = movie.rating;
+                    movieCounts[uniqueDirectorCount] = 1;
+                    uniqueDirectorCount++;
                 }
             }
-
-
-            if (!found) {
-                directors_unique[size] = director;
-                count_each_director[size] = 1;
-                size++;
-            }
-
 
             String bestDirector = "";
-            int maxCount = 0;
+            double highestAverage = -1.0;
 
-            for (int i = 0; i < size; i++) {
-                if (count_each_director[i] > maxCount) {
-                    maxCount = count_each_director[i];
-                    bestDirector = directors_unique[i];
+            for (int i = 0; i < uniqueDirectorCount; i++) {
+                double average = ratingSums[i] / movieCounts[i];
+                if (average > highestAverage) {
+                    highestAverage = average;
+                    bestDirector = directors[i];
                 }
             }
-            int best_movie_sum = 0;
-            int best_movie_num = 0;
-            for(Movie movie : movies) {
 
-                if (movie.director == bestDirector) {
-                    best_movie_num++;
-                    best_movie_sum += movie.rating;
-                }
-            }
-            int best_avg = best_movie_sum / best_movie_num;
-            System.out.println("Best director: " + bestDirector + "with an average rating of" + best_avg);
-            }
-
-
+            System.out.println("Best director: " + bestDirector + " with an average rating of " + highestAverage);
         }
-
     }
 }
