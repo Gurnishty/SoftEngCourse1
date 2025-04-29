09c0c0b7f8261ddca74472b20c96805423b2ea20
Ethantication <ethanorjoseph@gmail.com>
2025-04-29 21:39:00 +0300

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,8 +1,23 @@
+/**
+ * Represents a movie with a title, director, and rating.
+ */
 public class Movie {
+    /** Title of the movie. */
     String title;
+
+    /** Director of the movie. */
     String director;
+
+    /** Rating of the movie. */
     double rating;
 
+    /**
+     * Constructs a new Movie with the given title, director, and rating.
+     *
+     * @param title the name of the movie
+     * @param director the name of the director
+     * @param rating the movie's rating
+     */
     public Movie(String title, String director, double rating) {
         this.title = title;
         this.director = director;
